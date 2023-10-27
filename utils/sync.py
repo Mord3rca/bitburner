c50119583727ddb565928c230fef331ab2907ab4
Mord3rca <morderca@morderca.net>
2023-10-27 17:56:56 +0200

    Modernize sync.py

--- utils/sync.py
+++ utils/sync.py
@@ -1,41 +1,45 @@
 #!/usr/bin/env python3
 
-import os
-import sys
-import glob
-import requests
+from argparse import ArgumentParser
 from base64 import b64encode
+from pathlib import Path
+from requests import Session
+
+args = None
+sess = Session()
 
 
-def usage():
-	exit(f"Usage: {sys.argv[0]} [API Key]")
+def sendfile(file: Path) -> None:
+    print(f"Sending {file}")
+    data = b64encode(file.read_bytes())
+
+    # Send it to Bitburner
+    sess.post(args.url, json={
+        "code": data.decode(),
+        "filename": str(file.relative_to(args.path)),
+    })
 
 
-def sendfile(filename: str) -> None:
-	print(f"Sending {filename}")
-	# Read the content and encode it in base64
-	with open(filename, 'rb') as f:
-		data = b64encode(f.read())
-
-	# Send it to Bitburner
-	c.post("http://localhost:9990/",
-		json={
-			"filename": filename,
-			"code": data.decode(),
-		})
+def copy_to() -> None:
+    for i in Path(args.path).glob("**/*"):
+        if not i.is_dir():
+            sendfile(i)
 
 
 if __name__ == "__main__":
-	if(len(sys.argv) != 2):
-		usage()
+    parser = ArgumentParser(prog="sync", description="Sync files from/to bitburner game")
+    parser.add_argument(
+        "--url", default="http://localhost:9990/", type=str,
+        help="Set bitburner listenning URL"
+    )
+    parser.add_argument(
+        "--to", default=copy_to, action="store_const",
+        dest="action", help="Copy tree to bitburner game (default)"
+    )
+    parser.add_argument("api_key", type=str)
+    parser.add_argument("path", type=str, default=".")
+    args = parser.parse_args()
 
-	# Go to the root git directory
-	os.chdir(os.path.join(os.path.dirname(sys.argv[0]), '..'));
-
-	# Create the session and add the Authorization header
-	c = requests.Session()
-	c.headers["Authorization"] = f"Bearer {sys.argv[1]}"
-
-	for i in ['js', 'ns', 'txt']:
-		for j in glob.glob(f"./**/*.{i}", recursive=True):
-			sendfile(j.lstrip('./'))
+    # Create the session and add the Authorization header
+    sess.headers["Authorization"] = f"Bearer {args.api_key}"
+    args.action()
