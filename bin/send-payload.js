f34876538463647339e2cb7a738175372cbe10e9
Mord3rca <morderca@morderca.net>
2023-11-01 10:35:00 +0100

    Using conf for send-payload + exp-farm script

--- bin/send-payload.js
+++ bin/send-payload.js
@@ -1,21 +1,19 @@
 import {IniFile} from 'lib/config.js'
 import {getServerList} from 'lib/server.js'
 
-const PAYLOAD_NAME="payload.js"
-
 // Have to be >1 or it will only do weaken() call
 const MIN_SEC_MULT=1.2
 // Have to be <1 or it will only do grow() call
 const MAX_MONEY_MULT=0.8
 
-async function exec_payload(ns, server, extra_arg) {
-    let t = ( ns.getServerMaxRam(server.hostname) - ns.getServerUsedRam(server.hostname) ) / ns.getScriptRam(PAYLOAD_NAME)
+async function exec_default_payload(ns, server, payload, extra_arg) {
+    let t = ( ns.getServerMaxRam(server.hostname) - ns.getServerUsedRam(server.hostname) ) / ns.getScriptRam(payload)
     if(t < 1)
         return
 
     ns.tprintf("Exec payload on %s", server.hostname)
-    await ns.scp(PAYLOAD_NAME, server.hostname)
-    ns.exec(PAYLOAD_NAME, server.hostname, Math.floor(t), ...extra_arg)
+    await ns.scp(payload, server.hostname)
+    ns.exec(payload, server.hostname, Math.floor(t), ...extra_arg)
 }
 
 /** @param {NS} ns **/
@@ -23,9 +21,10 @@ export async function main(ns) {
     let cfg = new IniFile('/etc/target.txt')
     cfg.parse(ns)
     let target = cfg.get('Payload', 'target')
+    let payload = cfg.get('Payload', 'name', '/bin/payloads/default.js')
     let servers = (await getServerList(ns)).filter((s) => s.has_root)
     for(const i of servers) {
-        await exec_payload(ns, i, [
+        await exec_default_payload(ns, i, payload, [
             target,
             ns.getServerMinSecurityLevel(target) * MIN_SEC_MULT,
             ns.getServerMaxMoney(target) * MAX_MONEY_MULT
